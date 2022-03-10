ed16a3f923a679a66272d76605bf08f163fa5c11
MohammadReza Hassani <mohammad.mrho@gmail.com>
2022-03-10 17:39:52 +0330

    initial

---
+++ src/View/Routes.jsx
@@ -0,0 +1,11 @@
+import SignUp from "./Signup/SignUp";
+
+const routes = [
+    {
+        path: "/Signup",
+        name: "Sign up",
+        component: SignUp ,
+        layout: "/admin",
+    },
+];
+export default routes;
