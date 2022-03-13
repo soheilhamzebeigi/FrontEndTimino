d7bfbb740d85a02c41f42fbe7b38fb04d0e011c3
soheil hamzebeigi <45594725+soheilhamzebeigi@users.noreply.github.com>
2022-03-13 12:26:29 +0330

    Create code.component.js

---
+++ code.component.js
@@ -0,0 +1,70 @@
+import React, { Component } from 'react';
+import axios from "axios";
+
+export class Code extends Component {
+
+    state = {};
+
+    handleSubmit = e => {
+        e.preventDefault();
+        const data = {
+            code: this.code
+        };
+
+        axios.post('code', data).then(
+            res => {
+                this.setState({
+                    message: res.data.message,
+                    cls: 'success'
+                })
+            }
+        ).catch(
+            err => {
+                this.setState({
+                    message: err.response.data.message,
+                    cls: 'danger'
+                })
+            }
+        )
+    };
+
+    render() {
+        let message = '' ;
+
+        if(this.state.message){
+            const cls = 'alert alert-' + this.state.cls;
+            message = (
+                <div className={cls} role="alert">
+                    {this.state.message}
+                </div>
+            )
+        }
+
+        return (
+            <form onSubmit={this.handleSubmit}>
+                {message}
+                <h3>
+                    Code
+                </h3>
+
+                <div className="form-group">
+                    {/* <label>
+                         Code
+                    </label> */}
+                    <input type="code" className="form-control" placeholder="Code" onChange={e => this.code = e.target.value} />
+                </div>
+
+                <button className="btn btn-primary btn-block"> Submit</button> 
+
+                <p className="forgot-password text-right">
+                        <Link to={'/reset'}>Reset Password</Link>
+                </p>
+
+                {/* <button className="btn btn-primary btn-block" onClick={() => {
+                    alert.show('Oh look, an alert!')
+                    }}> Submit</button>  */}
+
+            </form>
+        )
+    }
+}
